c05d3158f950d18171efba4ae27bb5cf2b65d28a
salty-muffin <t.babel@gmx.com>
2025-04-11 21:08:45 +0200

    Add delete_files and list_files scripts for managing RAG corpus files

---
+++ vertex_ai_rag/delete_files.py
@@ -0,0 +1,30 @@
+import click
+from dotenv import dotenv_values
+from vertexai import rag
+import vertexai
+
+# Load environment variables from .env file
+config = dotenv_values(".env")
+
+
+# fmt: off
+@click.command()
+@click.option("--corpus-name", "-n", type=str, help="Name for the RAG corpus (format: projects/\{\}/locations/\{\}/ragCorpora/\{\})", requires=True)
+@click.argument("files", nargs=-1)
+# fmt: on
+def import_files(corpus_name, files) -> None:
+    """
+    Add files to RAG corpus with the given display name and data urls (Google Cloud Storage bucket and Google Drive Links).
+    """
+
+    # Initialize Vertex AI API once per session
+    vertexai.init(project=config["PROJECT_ID"], location=config["LOCATION"])
+
+    # Delete Files from the RagCorpus
+    for file in files:
+        rag.delete_file(file, corpus_name)
+        click.echo(f"Deleted file {file} from corpus.")
+
+
+if __name__ == "__main__":
+    import_files()
