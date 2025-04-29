a9faeaef119877ff37aa11291b49bccc311b926f
salty-muffin <t.babel@gmx.com>
2025-04-29 21:57:46 +0200

    added first sidebar

--- web-interface/src/routes/norbert/+layout.svelte
+++ web-interface/src/routes/norbert/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
-	import '../app.css';
-
 	import type { LayoutProps } from './$types';
+
+	import * as Sidebar from '$lib/components/ui/sidebar/index';
+	import ChatSidebar from '$lib/components/chat-sidebar.svelte';
 
 	let { children }: LayoutProps = $props();
 </script>
 
-{@render children()}
+<Sidebar.Provider>
+	<ChatSidebar />
+	<main>
+		<Sidebar.Trigger />
+		{@render children()}
+	</main>
+</Sidebar.Provider>
