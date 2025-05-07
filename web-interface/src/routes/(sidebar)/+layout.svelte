cd503710a82a83e36b2362c0893312dcc9009947
salty-muffin <t.babel@gmx.com>
2025-05-07 13:16:08 +0200

    Fix border style in layout component for consistent appearance

--- web-interface/src/routes/(sidebar)/+layout.svelte
+++ web-interface/src/routes/(sidebar)/+layout.svelte
@@ -25,7 +25,7 @@
 	<ChatSidebar />
 	<main class="relative flex min-h-screen w-full flex-col">
 		<div
-			class="bg-background border-foreground sticky top-0 z-10 flex w-full shrink-0 justify-between border-[1px] md:pointer-events-none md:border-none md:bg-transparent"
+			class="bg-background border-foreground sticky top-0 z-10 flex w-full shrink-0 justify-between border-b-[1px] md:pointer-events-none md:border-none md:bg-transparent"
 		>
 			<Sidebar.Trigger
 				bind:sidebarObj={sidebar}
