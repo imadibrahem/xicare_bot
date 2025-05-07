27cd5b80c9ef8c165eec4df5fce04b5712e1132a
salty-muffin <t.babel@gmx.com>
2025-05-07 13:14:11 +0200

    Fix border class name in message component and adjust padding in layout

--- web-interface/src/lib/components/message.svelte
+++ web-interface/src/lib/components/message.svelte
@@ -10,7 +10,7 @@
 	class={cn(
 		{ 'text-right': role === 'user' },
 		// { 'border-foreground border-[1px] border-dashed text-primary-foreground': role === 'user' },
-		{ 'border--dashed': role === 'user' },
+		{ '_border-dashed': role === 'user' },
 		role === 'user' ? 'self-end' : 'self-start',
 		'flex flex-col gap-0.5 p-4 md:mx-4 '
 	)}
@@ -21,7 +21,7 @@
 </div>
 
 <style>
-	.border--dashed {
+	._border-dashed {
 		background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='hsl(0 0 98%)' stroke-width='2' stroke-dasharray='14%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
 	}
 </style>
--- web-interface/src/routes/(article)/+layout.svelte
+++ web-interface/src/routes/(article)/+layout.svelte
@@ -2,6 +2,6 @@
 	let { children } = $props();
 </script>
 
-<main class="mx-auto my-9 max-w-screen-lg px-6">
+<main class="mx-auto my-9 max-w-screen-lg px-4 md:px-10">
 	{@render children()}
 </main>
