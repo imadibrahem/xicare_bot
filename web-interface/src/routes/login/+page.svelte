0d9816ffbf838e5ff7f0790414d6bd09636f9f0c
salty-muffin <t.babel@gmx.com>
2025-05-07 13:26:15 +0200

    Refactor login page to integrate login form directly

--- web-interface/src/routes/login/+page.svelte
+++ web-interface/src/routes/login/+page.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
-	import LoginForm from '$lib/components/login-form.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
 </script>
 
 <main class="flex h-screen w-screen items-center justify-center">
-	<LoginForm class="m-4" />
+	<Card.Root class="m-4 w-96">
+		<Card.Header>
+			<Card.Title class="text-2xl">Login</Card.Title>
+			<Card.Description>Enter your username below to log in</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<div class="grid gap-4">
+				<div class="grid gap-2">
+					<Label for="username">Username</Label>
+					<Input id="username" type="text" placeholder="SomeUser" required />
+				</div>
+				<div class="grid gap-2">
+					<div class="flex items-center">
+						<Label for="password">Password</Label>
+					</div>
+					<Input id="password" type="password" required />
+				</div>
+				<Button type="submit" class="w-full">Login</Button>
+			</div>
+		</Card.Content>
+	</Card.Root>
 </main>
