6cc9a7dd67d929a0d71f025a74604ba396beecba
salty-muffin <t.babel@gmx.com>
2025-05-07 13:14:01 +0200

    added login page

---
+++ web-interface/src/routes/login/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import LoginForm from '$lib/components/login-form.svelte';
+</script>
+
+<main class="flex h-screen w-screen items-center justify-center">
+	<LoginForm class="m-4" />
+</main>
