0984f17a2760c70d7dd37095f9f91237bbab6b0a
salty-muffin <t.babel@gmx.com>
2025-05-10 16:07:37 +0200

    Clear userId on logout to ensure proper session termination

--- web-interface/src/routes/logout/+server.ts
+++ web-interface/src/routes/logout/+server.ts
@@ -3,6 +3,7 @@ import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = ({ locals }) => {
 	locals.pb.authStore.clear();
+	locals.userId = undefined;
 
 	return redirect(303, '/login');
 };
