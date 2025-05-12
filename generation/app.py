92a9fbef148ea445f5ea30e2448ff03f7935381e
salty-muffin <t.babel@gmx.com>
2025-05-13 01:17:04 +0200

    broke out vertex ai rag model

--- generation/app.py
+++ generation/app.py
@@ -4,81 +4,29 @@ from fastapi.middleware.cors import CORSMiddleware
 import requests
 from pydantic import BaseModel
 import uvicorn
-
-import vertexai
-from vertexai import rag
-from vertexai.generative_models import (
-    GenerativeModel,
-    Tool,
-    GenerationConfig,
-    Part,
-    SafetySetting,
-    Content,
-)
-
 from dotenv import dotenv_values
+
+from generators.vertexai_rag import VertexAIRAG
+
 
 # Load environment variables from .env file
 config = dotenv_values(".env")
 
 # Load system prompt
-with open("server/system_prompt.txt") as file:
+with open("generation/system_prompt.txt") as file:
     system_prompt = file.read()
 
 
-def create_rag_model():
-    # Initialize Vertex AI with project and location from config
-    vertexai.init(project=config["PROJECT_ID"], location=config["LOCATION"])
-
-    # Get RAG corpus
-    rag_corpus = rag.get_corpus(config["RAG_CORPUS"])
-
-    # Direct context retrieval
-    rag_retrieval_config = rag.RagRetrievalConfig(
-        top_k=3,  # Optional
-        filter=rag.Filter(vector_distance_threshold=0.5),  # Optional
-    )
-
-    # Enhance generation
-    rag_retrieval_tool = Tool.from_retrieval(
-        retrieval=rag.Retrieval(
-            source=rag.VertexRagStore(
-                rag_resources=[
-                    rag.RagResource(
-                        rag_corpus=rag_corpus.name,  # Currently only 1 corpus is allowed.
-                        # Optional: supply IDs from `rag.list_files()`.
-                        # rag_file_ids=["rag-file-1", "rag-file-2", ...],
-                    )
-                ],
-                rag_retrieval_config=rag_retrieval_config,
-            ),
-        )
-    )
-
-    generate_content_config = GenerationConfig(
-        temperature=1,
-        top_p=1,
-        max_output_tokens=8192,
-    )
-
-    # Create a Gemini model instance
-    # return client, generate_content_config
-    return GenerativeModel(
-        model_name="gemini-2.0-flash-001",
-        tools=[rag_retrieval_tool],
-        system_instruction=Part.from_text(text=system_prompt),
-        generation_config=generate_content_config,
-        safety_settings=[
-            SafetySetting(category="HARM_CATEGORY_HATE_SPEECH", threshold="OFF"),
-            SafetySetting(category="HARM_CATEGORY_DANGEROUS_CONTENT", threshold="OFF"),
-            SafetySetting(category="HARM_CATEGORY_SEXUALLY_EXPLICIT", threshold="OFF"),
-            SafetySetting(category="HARM_CATEGORY_HARASSMENT", threshold="OFF"),
-        ],
-    )
-
-
 # Get RAG model
-norbert = create_rag_model()
+norbert = VertexAIRAG(
+    system_prompt=system_prompt,
+    project=config["PROJECT_ID"],
+    rag_corpus=config["RAG_CORPUS"],
+    location=config["LOCATION"],
+    temp=1.0,
+    top_p=1.0,
+    max_output_tokens=8192,
+)
 
 # Initializing FastAPI with CORS
 app = FastAPI()
@@ -119,22 +67,6 @@ def verify_token(token: str):
     return None
 
 
-def create_conversation_contents(items: dict[str, str]) -> list[Part]:
-    return [
-        Content(
-            role="user" if item["role"] == "user" else "model",
-            parts=[Part.from_text(text=item["text"])],
-        )
-        for item in items
-    ]
-
-
-# Dummy AI generator
-def run_ai(contents: list[Part]) -> str:
-    response = norbert.generate_content(contents)
-    return response.candidates[0].content.parts[0].text
-
-
 @app.post("/generate")
 async def generate(data: GenerateRequest, request: Request):
     # Extract token from request header
@@ -169,11 +101,8 @@ async def generate(data: GenerateRequest, request: Request):
         },
     )
 
-    # Create conversation parts
-    parts = create_conversation_contents(response.json()["items"])
-
     # Generate AI response
-    response_text = run_ai(parts)
+    response_text = norbert.generate_content(response.json()["items"])
 
     # Save AI's response to PocketBase
     requests.post(
