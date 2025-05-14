c7d8415cbca1ce9de6027a773b04ba828c272c2d
salty-muffin <t.babel@gmx.com>
2025-05-14 14:52:47 +0200

    made styles easier

--- web-interface/src/lib/components/chat-input.svelte
+++ web-interface/src/lib/components/chat-input.svelte
@@ -45,7 +45,7 @@
 	<form
 		class={cn(
 			className,
-			'bg-secondary border-foreground pointer-events-auto flex gap-x-4 border-[1px] p-4'
+			'bg-secondary border-foreground pointer-events-auto flex gap-x-4 rounded-xl border-[1px] p-4'
 		)}
 		{...rest}
 	>
--- web-interface/src/lib/components/message.svelte
+++ web-interface/src/lib/components/message.svelte
@@ -20,10 +20,9 @@
 <div
 	class={cn(
 		{ 'text-right': role === 'user' },
-		// { 'border-foreground border-[1px] border-dashed text-primary-foreground': role === 'user' },
-		{ '_border-dashed': role === 'user' },
+		{ 'bg-muted rounded-xl': role === 'user' },
 		role === 'user' ? 'self-end' : 'self-start',
-		'flex flex-col gap-0.5 p-4 md:mx-4',
+		'flex flex-col gap-0.5 px-4 py-2 md:mx-4',
 		className
 	)}
 	bind:this={ref}
@@ -34,9 +33,3 @@
 		>{new Date(created).toLocaleTimeString('de-DE', timeOptions)}</span
 	>
 </div>
-
-<style>
-	._border-dashed {
-		background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='hsl(0 0 98%)' stroke-width='2' stroke-dasharray='14%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
-	}
-</style>
