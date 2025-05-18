6fa2edb598590c1c407362f2aeb685a94e031787
salty-muffin <t.babel@gmx.com>
2025-05-18 11:08:44 +0200

    Add error page component for improved user feedback on errors

---
+++ web-interface/src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import ArrowLeft from '@lucide/svelte/icons/arrow-left';
+
+	import { page } from '$app/state';
+	import { currentUser } from '$lib/pocketbase.svelte';
+</script>
+
+<div class="flex h-screen w-full items-center justify-center">
+	<div class="flex items-center gap-4">
+		<a class="pt-1" href={currentUser.record ? '/' : '/login'}><ArrowLeft /></a>
+		<h1 class="text-2xl">
+			{page.status}{#if page.error}{' – '}{page.error.message}{/if}
+		</h1>
+	</div>
+</div>
