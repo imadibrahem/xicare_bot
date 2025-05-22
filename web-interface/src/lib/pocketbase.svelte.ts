05f13e2cb733e7f8ca891b29f3950b10fba3680e
salty-muffin <t.babel@gmx.com>
2025-05-22 17:25:09 +0200

    Fix PocketBase initialization to handle empty PUBLIC_PB_URL

--- web-interface/src/lib/pocketbase.svelte.ts
+++ web-interface/src/lib/pocketbase.svelte.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 import { PUBLIC_PB_URL } from '$env/static/public';
 
-export const pb = new PocketBase(PUBLIC_PB_URL);
+export const pb = new PocketBase(PUBLIC_PB_URL.length ? PUBLIC_PB_URL : '/');
 
 class User {
 	token = $state(pb.authStore.token);
