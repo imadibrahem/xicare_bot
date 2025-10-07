4d4ed09afa7efb319206a5d486467dae65f2015c
MoritzWeinSql <moritz.weinrich@sqlxpert.de>
2025-10-07 23:59:12 +0000

    needed final changes ips

--- run_create_germany_ips.sh
+++ run_create_germany_ips.sh
@@ -3,7 +3,10 @@
 # not needed
 # source /home/zenogries/berlin-chat-dev-interface/venv/bin/activate
 
+# cd to repo root
+cd /home/zenogries/berlin-chat-dev-interface
+
 # Run the Python script
-python /home/zenogries/berlin-chat-dev-interface/create_germany_ips.py
+python3 /home/zenogries/berlin-chat-dev-interface/create_germany_ips.py
 
 # reloading caddy is within .py and not here so that it does not get reloaded unnecessarily
