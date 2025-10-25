249c24d250c31f5105f98babc3bdfd4b230d423e
MoritzWeinSql <moritz.weinrich@sqlxpert.de>
2025-10-25 23:59:50 +0000

    dev system

--- run_dev_create_germany_ips.sh
+++ run_dev_create_germany_ips.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Activate the virtual environment
 # not needed
-# source /home/zenogries/berlin-chat-dev-interface/venv/bin/activate
+# source ~/berlin-chat-dev-interface/venv/bin/activate
 
 # cd to repo root
-cd /home/zenogries/berlin-chat-dev-interface
+cd ~/berlin-chat-dev-interface
 
 # Run the Python script
-python3 /home/zenogries/berlin-chat-dev-interface/create_germany_ips.py
+python3 ~/berlin-chat-dev-interface/create_germany_ips.py
 
 # reloading caddy is within .py and not here so that it does not get reloaded unnecessarily
